d75d320f993ab495395624fbb18013dd789245bd
Joseph Cooper <hambloko@gmail.com>
2015-11-03 20:53:31 -0600

    second push, stuff is now working

--- js/main.js
+++ js/main.js
@@ -3,34 +3,35 @@
 
 var gameRouter = Backbone.Router.extend({
   routes: {
-    "": "Menu",
+    "": "main",
+    "menu": "menu",
     "play": "play",
     "leaderboard": "leaderboard",
     "settings": "settings"
   },
-
+  main: function(){
+    var router = this;
+    $('.dunkel').html('<p>Loading...</p>');
+    setTimeout(function(){
+      router.navigate('menu', {trigger: true});
+    }, 4000);
+  },
+  menu: function() {
+    $('.dunkel').html('<a href="#play">Play</a> ' + '<a href="#leaderboard">leaderboard</a> ' + '<a href="#settings">Settings</a>')
+  },
 
   play: function() {
-     $('.dunkel').html('');
-     $('.dunkel').append('<img class="image" src="http://themetalreview.com/wp-content/uploads/2015/04/0.jpeg">');
+     $('.dunkel').html('<img class="image" src="http://themetalreview.com/wp-content/uploads/2015/04/0.jpeg">');
   },
 
   leaderboard: function() {
-    $('.dunkel').html('');
-    $('.dunkel').append('<img class="image" src="http://image.automotive.com/f/news/study-bad-drivers-listen-to-nickelback-and-the-black-eyed-peas-124159/62853213+w968/chad-kroeger-nickelback-terrible-song-driving.jpg">');
-
-
+    $('.dunkel').html('<img class="image" src="http://image.automotive.com/f/news/study-bad-drivers-listen-to-nickelback-and-the-black-eyed-peas-124159/62853213+w968/chad-kroeger-nickelback-terrible-song-driving.jpg">');
   },
 
   settings: function() {
-    $('.dunkel').html('');
-    $('.dunkel').append('<img class="image" src="http://cdn2.crushable.com/wp-content/uploads/2012/08/chadkroeger.jpeg">');
+    $('.dunkel').html('<img class="image" src="http://cdn2.crushable.com/wp-content/uploads/2012/08/chadkroeger.jpeg">');
   }
 
-  setTimeout(goAway({
-    $('.dunkel').html('');
-    $('.dunkel').append('<section class="loading">Loading...</section>');
-  }) 4000);
 });
 
 var router = new gameRouter();
